f9f09939813284e24d8b8b0ede9f3891917b9a0f
vamsi-krish-chiguruwada <vamsikrishchiguruwada@gmail.com>
2024-10-15 01:06:37 -0400

    New Resume and fixes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -37,8 +37,8 @@ function ResumeNew() {
             className="d-flex flex-column align-items-center "
           >
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
-            <Page pageNumber={3} scale={width > 786 ? 1.7 : 0.6} />
+            {/* <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={3} scale={width > 786 ? 1.7 : 0.6} /> */}
           </Document>
         </Row>
 
